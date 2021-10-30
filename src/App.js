6c8f48775b5b2f071639b85551bcef085f13fa2c
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2021-10-30 19:04:06 -0300

    timer funcionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Timer } from './Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.startTime = this.startTime.bind(this);
+    this.handleStopTime = this.handleStopTime.bind(this);
+    this.handleDificult = this.handleDificult.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.finishTime = this.finishTime.bind(this);
+
+    this.dificult = {
+      hard: 15,
+      medium: 10,
+      easy: 0,
+    };
+
+    this.state = {
+      seconds: 0,
+      mostraMensagem: false,
+      dificult: 'easy',
+      stopTimer: false,
+      resetTimer: false,
+    };
+  }
+
+  componentDidMount() {
+    this.startTime();
+  }
+
+  startTime() {
+    const seconds = 30 - this.dificult[this.state.dificult];
+
+    this.setState({
+      seconds: seconds,
+    });
+  }
+
+  handleStopTime() {
+    this.setState({ stopTimer: true });
+  }
+
+  handleReset() {
+    this.setState({
+      resetTimer: !this.state.resetTimer,
+      stopTimer: false,
+      mostraMensagem: false,
+    });
+  }
+
+  handleDificult(dificultName) {
+    this.setState({ stopTimer: true });
+
+    this.setState({ dificult: dificultName });
+    const seconds = 30 - this.dificult[dificultName];
+    this.setState({
+      seconds: seconds,
+      stopTimer: false,
+    });
+  }
+  finishTime() {
+    this.setState({ mostraMensagem: true });
+  }
+
+  render() {
+    const { seconds, resetTimer, stopTimer, dificult, mostraMensagem } =
+      this.state;
+
+    return (
+      <>
+        {seconds !== 0 && (
+          <Timer
+            seconds={seconds}
+            resetTimer={resetTimer}
+            stopTimer={stopTimer}
+            finishTime={this.finishTime}
+          />
+        )}
+        {mostraMensagem && <p>Acabou o tempo</p>}
+        Dificuldade: {dificult}
+        <br />
+        <br />
+        <button type='button' onClick={this.handleStopTime}>
+          Stop
+        </button>
+        <button type='button' onClick={this.handleReset}>
+          Reset
+        </button>
+        <br />
+        <br />
+        <button type='button' onClick={() => this.handleDificult('easy')}>
+          Facil
+        </button>
+        <button type='button' onClick={() => this.handleDificult('medium')}>
+          Normal
+        </button>
+        <button type='button' onClick={() => this.handleDificult('hard')}>
+          Dificil
+        </button>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Timer.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+export class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.timer = {};
+
+    this.state = {
+      seconds: this.props.seconds,
+      resetTimer: this.props.resetTimer,
+    };
+    this.countDown = this.countDown.bind(this);
+    this.changeSeconds = this.changeSeconds.bind(this);
+  }
+
+  componentDidMount() {
+    this.countDown();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { stopTimer, seconds, resetTimer } = this.props;
+
+    //reset timer
+    if (resetTimer !== prevProps.resetTimer) {
+      clearInterval(this.timer);
+      this.changeSeconds();
+    }
+
+    //change timer
+    if (prevProps.seconds !== seconds) {
+      clearInterval(this.timer);
+      this.changeSeconds();
+    }
+
+    //stop timer
+    if (stopTimer) {
+      return clearInterval(this.timer);
+    }
+  }
+
+  changeSeconds() {
+    this.countDown();
+  }
+
+  countDown() {
+    this.setState({ seconds: this.props.seconds });
+    const { finishTime } = this.props;
+
+    this.timer = setInterval(() => {
+      this.setState({ seconds: this.state.seconds - 1 });
+      if (this.state.seconds === 0) {
+        finishTime();
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
+  render() {
+    const { seconds } = this.state;
+    return <div>{seconds}</div>;
+  }
+}
